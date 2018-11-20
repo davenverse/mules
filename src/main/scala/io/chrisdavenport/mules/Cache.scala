c402b3c2fb1d51610a23a03cee28cc1372fc75b4
Gabriel Volpe <volpegabriel@gmail.com>
2018-11-20 15:35:55 +0900

    Adding Auto-Cache: Supports automatic key expiration via spinning up a background process.

--- src/main/scala/io/chrisdavenport/mules/Cache.scala
+++ src/main/scala/io/chrisdavenport/mules/Cache.scala
@@ -1,10 +1,13 @@
 package io.chrisdavenport.mules
 
+import java.util.concurrent.TimeUnit
+
 import cats._
 import cats.data.OptionT
-import cats.effect.{Sync, Clock}
+import cats.effect._
 // For Cats-effect 1.0
 import cats.effect.concurrent.Ref
+import cats.effect.syntax.concurrent._
 import cats.implicits._
 import scala.concurrent.duration._
 import scala.collection.immutable.Map
@@ -81,8 +84,8 @@ class Cache[F[_], K, V] private[Cache] (
 
 object Cache {
   // Value of Time In Nanoseconds
-  class TimeSpec private (
-    val nanos: Long
+  case class TimeSpec private (
+    nanos: Long
   ) extends AnyVal
   object TimeSpec {
 
@@ -98,11 +101,38 @@ object Cache {
     def unsafeFromNanos(l: Long): TimeSpec =
       new TimeSpec(l)
 
+    implicit class TimeSpecOps(val timeSpec: TimeSpec) {
+      def toDuration: FiniteDuration =
+        Duration(timeSpec.nanos, TimeUnit.NANOSECONDS)
+    }
+
   }
   private case class CacheItem[A](
     item: A,
     itemExpiration: Option[TimeSpec]
   )
+
+  /**
+     * Creates a new cache with default expiration and automatic key-expiration support.
+    *
+    * It fires off a background process that checks for expirations every certain amount of time.
+    *
+    * @param expiresIn: the expiration time of every key-value in the Cache.
+    * @param checkOnExpirationsEvery: how often the expiration process should check for expired keys.
+    *
+    * @return an `[F[Cache[F, K, V]]` that will create a Cache with key-expiration support when evaluated.
+    * */
+    def createAutoCache[F[_]: Clock: Concurrent: Timer, K, V](
+        expiresIn: TimeSpec,
+        checkOnExpirationsEvery: TimeSpec
+   ): F[Cache[F, K, V]] = {
+     def runExpiration(cache: Cache[F, K, V]): F[Unit] =
+       Timer[F].sleep(checkOnExpirationsEvery.toDuration) >> purgeExpired(cache) >> runExpiration(cache)
+
+      Ref.of[F, Map[K, CacheItem[V]]](Map.empty[K, CacheItem[V]])
+       .map(ref => new Cache[F, K, V](ref, Some(expiresIn)))
+       .flatTap(runExpiration(_).start.void)
+     }
 
   /**
     * Create a new cache with a default expiration value for newly added cache items.
