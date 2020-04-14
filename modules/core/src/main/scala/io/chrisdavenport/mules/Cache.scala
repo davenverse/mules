01f610a6b1feb164987dfa858f7f834cd6a122e3
David Strawn <isomarcte@gmail.com>
2020-04-14 13:02:51 -0600

    Add Traits For Timeout Based Inserts
    * `modules/core/src/main/scala/io/chrisdavenport/mules/Cache.scala`
        * Added `LifetimeInsert` and `LifetimeCache` which define `defaultExpiration` and `insertWithTimeout`.
    * `modules/core/src/main/scala/io/chrisdavenport/mules/DispatchOneCache.scala`
        * Implement `LifetimeCache` rather than `Cache`.
        * Remove `insert` and rely on the default implementation in `LifetimeInsert`.
            * `insert` was already the same as `insertWithTimeout`, except it used the `defaultExpiration`.
    * `modules/core/src/main/scala/io/chrisdavenport/mules/MemoryCache.scala`
        * Implement `LifetimeCache` rather than `Cache`.
        * Remove `insert` and rely on the default implementation in `LifetimeInsert`.
            * `insert` was already the same as `insertWithTimeout`, except it used the `defaultExpiration`.
    * `modules/reload/src/main/scala/io/chrisdavenport/mules/reload/AutoFetchingCache.scala`
        * Implement `Get`, `LifetimeInsert`, in addition to `Lookup`.
            * `Get` seems as though it should have already been implemented, since `lookupCurrent` is exactly the same as `Get`.
            * Deprecate `lookupCurrent`, since it is exactly the same as `get`.
            * Use the default implementation for `insert`.
            * Make `insert` `public`. It appears to have been mistakenly made private, as all it does is call `insertWithTimeout(defaultExpiration)(k, v)` and `insertWithTimeout` is `public`.

--- modules/core/src/main/scala/io/chrisdavenport/mules/Cache.scala
+++ modules/core/src/main/scala/io/chrisdavenport/mules/Cache.scala
@@ -12,11 +12,40 @@ trait Insert[F[_], K, V]{
   def insert(k: K, v: V): F[Unit]
 }
 
+/** As [[Insert]] but allows setting an expiration time for inserts.
+  *
+  * The default implementation of [[#insert]] will use the
+  * [[#defaultExpiration]] value, but concrete implementations ''may''
+  * override this.
+  */
+trait LifetimeInsert[F[_], K, V] extends Insert[F, K, V]{
+
+  /** The default expiration time for values inserted with the [[#insert]] method. */
+  def defaultExpiration: Option[TimeSpec]
+
+  /**
+    * Insert an item in the cache, with an explicit expiration value.
+    *
+    * If the expiration value is None, the item will never expire. The default expiration value of the cache is ignored.
+    *
+    * The expiration value is relative to the current clockMonotonic time, i.e. it will be automatically added to the result of clockMonotonic for the supplied unit.
+    **/
+  def insertWithTimeout(optionTimeout: Option[TimeSpec])(k: K, v: V): F[Unit]
+
+  override def insert(k: K, v: V): F[Unit] =
+    this.insertWithTimeout(this.defaultExpiration)(k, v)
+}
+
 trait Delete[F[_], K]{
   def delete(k: K): F[Unit]
 }
 
-trait Cache[F[_], K, V] 
+trait Cache[F[_], K, V]
   extends Lookup[F, K, V]
   with Insert[F, K, V]
   with Delete[F, K]
+
+/** As [[Cache]] but additionally supports [[#insertWithTimeout]]. */
+trait LifetimeCache[F[_], K, V]
+    extends Cache[F, K, V]
+    with LifetimeInsert[F, K, V]
